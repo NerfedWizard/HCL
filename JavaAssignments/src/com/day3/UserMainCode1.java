a7e7ba3da6ed71262c9755126dc3bb9a17e1274f
NerfedWizard <loelnelson@gmail.com>
2021-11-06 14:34:56 -0500

    Q9D3

--- JavaAssignments/src/com/day3/UserMainCode1.java
+++ JavaAssignments/src/com/day3/UserMainCode1.java
@@ -1,10 +1,25 @@
 package com.day3;
 
-/** Qyestion 9 */
+/**
+ * Question 9 Take a string and reverse it and place a char in between each
+ * character
+ */
 public class UserMainCode1 {
-
 	public static void main(String[] args) {
-
+		char a = '+';
+		String rabbit = "rabbit";
+		System.out.println(reverseAddChar(rabbit, a));
 	}
 
+	public static String reverseAddChar(String old, char sep) {
+		String result = "";
+		char[] chngd = old.toLowerCase().toCharArray();
+		for (int i = chngd.length - 1; i >= 0; i--) {
+			result += chngd[i];
+			if (i != 0) {
+				result += sep;
+			}
+		}
+		return result;
+	}
 }
