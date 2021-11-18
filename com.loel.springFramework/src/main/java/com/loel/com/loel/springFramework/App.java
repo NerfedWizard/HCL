203e265dd41fe18c764ac4c1c30c587fceb07042
NerfedWizard <loelnelson@gmail.com>
2021-11-18 11:18:28 -0600

    SpringFramework

---
+++ com.loel.springFramework/src/main/java/com/loel/com/loel/springFramework/App.java
@@ -0,0 +1,13 @@
+package com.loel.com.loel.springFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
